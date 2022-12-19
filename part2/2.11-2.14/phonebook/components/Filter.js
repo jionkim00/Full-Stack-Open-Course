f58bb4fe976c50a8f342125bda05d4aeff511538
Jion <62360386+jionkim00@users.noreply.github.com>
2022-12-19 14:30:27 -0800

    Add files via upload

---
+++ part2/2.11-2.14/phonebook/components/Filter.js
@@ -0,0 +1,10 @@
+const Filter = ({filter, handleFilter}) => {
+    
+    return (
+        <form >
+            filter shown with: <input value={filter} onChange={handleFilter} />
+        </form>
+    )
+  }
+
+export default Filter
